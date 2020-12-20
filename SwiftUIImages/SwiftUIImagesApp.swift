7c15bf7030923d7cfe3d714852b5039d43a63eac
Your mato <you@example.com>
2020-12-20 18:39:53 +0800

    Initial Commit

---
+++ SwiftUIImages/SwiftUIImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIImagesApp.swift
+//  SwiftUIImages
+//
+//  Created by kittawat phuangsombat on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
